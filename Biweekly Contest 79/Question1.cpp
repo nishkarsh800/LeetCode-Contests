0f9b0428269402508742ae3a13f316452b604ae3
Rakesh Roshan <roshanrakesh7362@gmail.com>
2022-08-04 12:07:56 +0530

    Update Question1.cpp

--- Biweekly Contest 79/Question1.cpp
+++ Biweekly Contest 79/Question1.cpp
@@ -31,21 +31,28 @@ n == num.length
 1 <= n <= 10
 num consists of digits. */
 
-// Approach
+// Approach :-- Recursive Approach
 
-You are given a 0-indexed string num of length n consisting of digits.
-Return true if for every index i in the range 0 <= i < n, the digit i occurs num[i] times in num.
-Otherwise return false.
+1. First look up at the given statements and approach to the question.
+2. You are given a 0-indexed string num of length n consisting of digits.
+3. You should return true if for every index i in the range 0 <= i < n.
+4. Then look the digit i occurs num[i] times in num.
+5. If not otherwise return false.
 
-class Solution {
+class Solution
+{
 public:
-    bool digitCount(string num) {
+    bool digitCount(string num)
+    {
         vector<int> cnt(10);
-        for (const auto& x : num) {
+        for (const auto& x : num)
+        {
             ++cnt[x - '0'];
         }
-        for (int i = 0; i < size(num); ++i) {
-            if (cnt[i] != num[i] - '0') {
+        for (int i = 0; i < size(num); ++i)
+        {
+            if (cnt[i] != num[i] - '0')
+            {
                 return false;
             }
         }
